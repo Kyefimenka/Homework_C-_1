a7f102b2b562e386a6aab0e97715f25b5d7d4109
Katsiaryna <caelpi85@gmail.com>
2022-06-24 21:49:30 +0200

    Even number printing

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Enter number ");
+int N = Convert.ToInt32(Console.ReadLine());
+int curent = 2;
+while (curent <= N)
+{ 
+    Console.WriteLine(curent);
+    curent += 2;
+}
